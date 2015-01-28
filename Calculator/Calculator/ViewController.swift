45ffa173c82b0493ea91a696b3254aa886960d8c
XiaoBangqiang <xiaobangqiang@XiaoBangqiangdeMacBook-Pro.local>
2015-01-29 00:08:56 +0800

    2. More Xcode and Swift,MAC

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -28,5 +28,71 @@ class ViewController: UIViewController
         
     }
    
+    
+    @IBAction func operate(sender: UIButton) {
+        
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber{
+            enter()
+        }
+        switch operation{
+//        case "×":preformOperation(multiply)
+//        case "×":preformOperation({ (op1:Double,op2:Double) ->Double in
+//            return op1 * op2 })
+//        case "×":preformOperation({ (op1,op2) in return op1 * op2 })
+//            case "×":preformOperation({ $0 * $1 })
+        case "×":preformOperation { $0 * $1 }
+        case "÷":preformOperation { $1 / $0 }
+        case "+":preformOperation { $0 + $1 }
+        case "−":preformOperation { $1 - $0 }
+        case "√":preformOperation {sqrt($0)}
+        default: break
+        }
+    }
+
+    func preformOperation(operation:(Double,Double) -> Double){
+    
+        if operandStack.count >= 2{
+            displayValue = operation(operandStack.removeLast(),operandStack.removeLast())
+            enter()
+
+        }
+    }
+    
+    
+    func preformOperation(operation:(Double) -> Double){
+        
+        if operandStack.count >= 1{
+            displayValue = operation(operandStack.removeLast())
+            enter()
+            
+        }
+    }
+    
+    
+    func multiply(op1:Double,op2:Double) ->Double {
+        return op1 * op2
+    }
+        
+    var operandStack = Array<Double>()
+    
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        println("operandStack = \(operandStack)")
+    }
+        
+    var displayValue: Double {
+        get{
+        
+        return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    
+    }
 }
 
